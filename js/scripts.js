86dfda70cba23a59621c5a8322881aa271ea36e7
CameronWill <cellardoorsolar@aol.com>
2020-08-20 02:26:08 -0700

    fix name append

--- js/scripts.js
+++ js/scripts.js
@@ -14,19 +14,19 @@ $(document).ready(function() {
       $(alert("You're Going To Need More Interest Than That!"))
       location.reload();
     } else if (media === "netflix") {
-      $("#fullName").append(fullNameInput);
+      $(".fullName").append(fullNameInput);
       $(".ruby").hide()
       $(".cSharp").hide();
       $(".fullStack").show()
       $("#btnRefresh").show()
       } else if (media === "hulu") {
-      $("#fullName").append(fullNameInput);
+      $(".fullName").append(fullNameInput);
       $(".cSharp").hide();
       $(".fullStack").hide()
       $(".ruby").show()
       $("#btnRefresh").show()
       } else if (media === "prime") {
-      $("#fullName").append(fullNameInput);
+      $(".fullName").append(fullNameInput);
       $(".fullStack").hide()
       $(".ruby").hide()
       $(".cSharp").show();
