0045d66a522fec7481a7b18c67281124e807b7e2
CameronWill <cellardoorsolar@aol.com>
2020-08-19 22:19:28 -0700

    fix interest alert, fix submit button function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,35 @@
 $(document).ready(function() {
   $("#program").submit(function(event) {
-    const fullName = $("input#fullName").val();
+    const fullNameInput = $("input#fullName").val();
     const interest = $("input#interest").val();
     const career = $("select#career").val();
-    const location = $("input#location").val();
+    const loc = $("input#loc").val();
     const salary = $("input#salary").val();
     const media = $("select#media").val();
 
-    if(fullName === "") {
+    if(fullNameInput === "") {
       $(alert("Please Enter Your Full Name!"))
       location.reload();
-    } else if (interest === interest < 5) {
+    } else if (interest <= 5) {
       $(alert("You're Going To Need More Interest Than That!"))
       location.reload();
     } else if (media === "netflix") {
-      $("#fullName").append(fullName);
-      $("#fullStack").show()
+      $("#fullName").append(fullNameInput);
+      $(".ruby").hide()
+      $(".cSharp").hide();
+      $(".fullStack").show()
       } else if (media === "hulu") {
-      $("#fullName").append(fullName);
-      $("#ruby").show()
+      $("#fullName").append(fullNameInput);
+      $(".cSharp").hide();
+      $(".fullStack").hide()
+      $(".ruby").show()
       } else if (media === "prime") {
-      $("#fullName").append(fullName);
-      $("#cSharp").show();
+      $("#fullName").append(fullNameInput);
+      $(".fullStack").hide()
+      $(".ruby").hide()
+      $(".cSharp").show();
       }
+      
 
     event.preventDefault()
   });
